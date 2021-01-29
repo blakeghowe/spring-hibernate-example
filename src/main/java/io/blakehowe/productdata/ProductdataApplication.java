fe419efa1ef4563e8f2ae9cfe7c9171cb7ec1fcc
Blake Howe <blake.g.howe@gmail.com>
2021-01-29 13:22:53 -0500

    initial project creation

---
+++ src/main/java/io/blakehowe/productdata/ProductdataApplication.java
@@ -0,0 +1,13 @@
+package io.blakehowe.productdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductdataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/blakehowe/productdata/ProductdataApplicationTests.java
@@ -0,0 +1,13 @@
+package io.blakehowe.productdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
